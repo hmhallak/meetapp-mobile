90e6e313d7d5815af93ae3bb44249412fd1b7fb1
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-10-21 23:01:08 -0400

    adds background config

---
+++ src/components/Background/index.js
@@ -0,0 +1,8 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+  colors: ['#23212d', '#412945'],
+})`
+  flex: 1;
+`;
